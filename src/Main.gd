b6a184953cb287812a5f54b1e7f449f7af4c35f6
aCube <player559001@gmail.com>
2022-03-31 21:22:04 -0300

    Improve player movement. Add player animations(jump, run and fall)

--- src/Main.gd
+++ src/Main.gd
@@ -6,17 +6,15 @@ var player_info := """
 Player:
 Position: {pos_x} | {pos_y}
 Velocity: {vel_x} | {vel_y}
-Motion: {mot_x} | {mot_y}	
 """
 
 func _ready() -> void:
 	pass
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	$UI.offset = $PinkMan/Camera.offset
 
 	$UI/Debug/PlayerInfo.text = player_info.format({
 		"pos_x": player.position.x, "pos_y": player.position.y,
 		"vel_x": player.velocity.x, "vel_y": player.velocity.y,
-		"mot_x": player.motion.x, "mot_y": player.motion.y,
 	})
--- src/player/PinkMan.gd
+++ src/player/PinkMan.gd
@@ -1,50 +1,87 @@
 extends KinematicBody2D
 
-const GRAVITY: int = 1200
-const MAX_SPEED: int = 350
+onready var Sprites := $Sprites
+onready var Animator := $Sprites/Animator
+onready var StateMachine: AnimationNodeStateMachinePlayback = $Sprites/AnimatorTree.get("parameters/playback")
+onready var JumpTimer := $JumpTimer
 
-export(int) var jump_height := 320
 export(int) var acceleration := 450
-export(float) var friction := 0.4
-export(float) var stop_friction := 0.8 
+export(float) var friction := 24.0
+export(float) var stop_friction := 48.0 
+export(float) var time_to_jump_peak := 0.3
+export(int) var jump_height := 64
+export(int) var jump_distance := 128
 
+const SNAP_VECTOR := Vector2.DOWN * 32.0
+
+var max_speed: float
+var gravity: float
+var jump_force: float
 var velocity := Vector2.ZERO
 var motion := Vector2.ZERO
+var current_snap := SNAP_VECTOR
+var last_x_motion := motion.x
+var can_jump := false
 
 func _ready() -> void:
-	pass
+	gravity = (2 * jump_height) / pow(time_to_jump_peak, 2)
+	jump_force = gravity * time_to_jump_peak
+	max_speed = jump_distance / (2.0 * time_to_jump_peak)
+
+func _on_JumpTimer_timeout() -> void:
+	can_jump = false
 
 func _physics_process(delta: float) -> void:
 	movement(delta)
-	
-	velocity.y = move_and_slide(velocity, Vector2.UP).y
+	velocity.y = move_and_slide_with_snap(velocity, current_snap, Vector2.UP).y
 
-func movement(delta: float) -> void:
-	var last_x = motion.x # Last X Motion
+func _process(_delta: float) -> void:
+	last_x_motion = motion.x
 	motion.x = 0
-	motion.y = 0
+
+	if is_on_floor():
+		can_jump = true
+	elif not can_jump and JumpTimer.is_stopped():
+		JumpTimer.start()
 
 	if Input.is_action_pressed("move_right"):
 		motion.x += 1
 	if Input.is_action_pressed("move_left"):
 		motion.x -= 1
 
-	if Input.is_action_just_pressed("move_jump") and is_on_floor():
-		velocity.y = -jump_height
+	if Input.is_action_just_pressed("move_jump") and can_jump:
+		velocity.y = -jump_force
+	if Input.is_action_just_released("move_jump"):
+		velocity.y -= velocity.y / 2
 
+	animation()
+
+func animation() -> void:
+	if motion.x < 0:
+		Sprites.flip_h = true
+	elif motion.x > 0:
+		Sprites.flip_h = false
+
+	if motion.x != 0 and is_on_floor():
+		StateMachine.travel("run")
+	elif velocity.y != 0:
+		if velocity.y < 0:
+			StateMachine.travel("jump")
+		else:
+			StateMachine.travel("fall")
+	else:
+		StateMachine.travel("idle")
+
+func movement(delta: float) -> void:
 	if motion.x != 0:
 		velocity.x += acceleration * motion.x * delta
-		velocity.x = clamp(velocity.x, -MAX_SPEED, MAX_SPEED)
-		if last_x != motion.x:
-			velocity.x = int(lerp(velocity.x, 0, stop_friction))
+		velocity.x = clamp(velocity.x, -max_speed, max_speed)
+		if last_x_motion != motion.x:
+			velocity.x = int(lerp(velocity.x, 0, stop_friction * delta))
 
 	if is_on_floor():
 		if motion.x == 0:
-			velocity.x = int(lerp(velocity.x, 0, friction))
+			velocity.x = int(lerp(velocity.x, 0, friction * delta))
 
-	velocity.y += GRAVITY * delta
-	if Input.is_action_just_released("move_jump") or is_on_ceiling():
-		velocity.y = 0
-
-#	velocity.y = motion.y if motion.y != 0 else velocity.y
-
+	current_snap = SNAP_VECTOR if velocity.y > 0 else Vector2.ZERO
+	velocity.y += gravity * delta
